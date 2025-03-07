cdc5768d98a2ceecbc491abe3c0e94a4633c02cd
LesCrow <john.baruteau@gmail.com>
2025-03-07 14:46:58 +0100

    wip

--- app/components/auth/SigninButton.tsx
+++ app/components/auth/SigninButton.tsx
@@ -5,7 +5,7 @@ import { signIn } from "next-auth/react";
 const SignInButton = () => {
   const handleSignIn = () => {
     // Sign in avec le fournisseur (ici GitHub)
-    signIn("github"); // Vous pouvez remplacer 'github' par d'autres fournisseurs comme 'google', etc.
+    signIn("strava"); // Vous pouvez remplacer 'github' par d'autres fournisseurs comme 'google', etc.
   };
 
   return <button onClick={handleSignIn}>Sign In with GitHub</button>;
--- lib/authOptions.ts
+++ lib/authOptions.ts
@@ -9,16 +9,15 @@ import Strava from "next-auth/providers/strava";
 export const authOptions: NextAuthConfig = {
   adapter: PrismaAdapter(prisma),
   providers: [
-    // Strava({
-    //   clientId: process.env.STRAVA_CLIENT_ID,
-    //   clientSecret: process.env.STRAVA_CLIENT_SECRET,
-    //   authorization:
-    //     "https://www.strava.com/oauth/authorize?scope=read,activity:read_all",
-    //   token: "https://www.strava.com/oauth/token",
-    //   userinfo: "https://www.strava.com/api/v3/athlete",
+    Strava({
+      clientId: process.env.STRAVA_CLIENT_ID,
+      clientSecret: process.env.STRAVA_CLIENT_SECRET,
+      authorization: "https://www.strava.com/oauth/authorize",
+      token: "https://www.strava.com/oauth/token",
+      userinfo: "https://www.strava.com/api/v3/athlete",
 
-    //   name: "strava",
-    // }),
+      name: "strava",
+    }),
     GithubProvider({
       clientId: process.env.GITHUB_ID as string,
       clientSecret: process.env.GITHUB_SECRET as string,
